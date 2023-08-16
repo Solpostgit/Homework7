08450577ad0e662873f7d06652eaf2dc2dab97a3
Solpostgit <begemot_off@mail.ru>
2023-08-16 20:04:30 +0300

    Added task1

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,13 @@ public class Main {
 
     public static void task1() {
         System.out.println("Task 1");
-
+        int month = 0;
+        int salary = 0;
+        while (salary < 2_459_000) {
+            salary = salary + 15_000;
+            month++;
+            System.out.printf("Месяц %d, сумма накоплений равна %d рублей \n", month, salary);
+        }
+        System.out.println();
     }
 }
